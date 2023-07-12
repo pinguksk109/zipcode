4a2d175c448b4a2c3678c37ac2235c81f6a6a297
Keisuke Ota <k.ota109@outlook.com>
2023-07-13 01:10:33 +0900

    Controllerのテストを記載

--- src/test/java/jp/co/ot/zipcode/presentation/controller/ZipcodeControllerTest.java
+++ src/test/java/jp/co/ot/zipcode/presentation/controller/ZipcodeControllerTest.java
@@ -1,15 +1,17 @@
 package jp.co.ot.zipcode.presentation.controller;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.Before;
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockserver.model.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -28,22 +30,38 @@ class ZipcodeControllerTest extends ZipcodeController {
 	@Mock
 	private ZipcodeService zipcodeService;
 	
-    @Before
+    @BeforeEach
     public void setUp() {
-      MockitoAnnotations.initMocks(this);
-      mvc = MockMvcBuilders.standaloneSetup(sut).build();
+    	MockitoAnnotations.initMocks(this);
+    	mvc = MockMvcBuilders.standaloneSetup(sut).build();
     }
 
 	
 	@Test
-	public void searchAddress_7桁の数字を受け取った場合_HTTPステータス200を返すこと() {
+	public void searchAddress_7桁の数字を受け取った場合_HTTPステータス200を返すこと() throws Exception {
         String zipcode = "0790177";
         mvc.perform(MockMvcRequestBuilders.get("/zipcode")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(zipcode))
-                .andExpect(status().isOk());
+        		.param("zipcode", zipcode))
+        		.andExpect(status().isOk());
 	}
 	
-	
+	@Test
+	public void searchAddress_7桁の数字以外を受け取った場合_HTTPステータス400を返すこと() throws Exception {
+        String zipcode = "079017";
+        mvc.perform(MockMvcRequestBuilders.get("/zipcode")
+        		.param("zipcode", zipcode))
+        		.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	public void searchAddress_エラーを受け取った場合_HTTPステータス500を返すこと() throws Exception {
+        String zipcode = "0790177";
+        String expectedErrorMessage = "問題が発生しました";
+        when(zipcodeService.searchAddress(zipcode)).thenThrow(new IOException(expectedErrorMessage));
+        mvc.perform(MockMvcRequestBuilders.get("/zipcode")
+        		.param("zipcode", zipcode))
+        		.andExpect(status().isInternalServerError());
+	}
+
 
 }
