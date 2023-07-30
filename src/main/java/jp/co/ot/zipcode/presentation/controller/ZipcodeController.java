ea182c4adfefbf350d281f1df15dc14d7b938cda
Keisuke Ota <k.ota109@outlook.com>
2023-07-30 13:37:18 +0900

    Controllerクラスでエラーハンドリングを修正

--- src/main/java/jp/co/ot/zipcode/presentation/controller/ZipcodeController.java
+++ src/main/java/jp/co/ot/zipcode/presentation/controller/ZipcodeController.java
@@ -1,12 +1,14 @@
 package jp.co.ot.zipcode.presentation.controller;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,7 @@ import jp.co.ot.zipcode.domain.model.ErrorResponse;
 import jp.co.ot.zipcode.domain.model.request.AddressEntity;
 
 @RestController
+@Validated
 @RequestMapping("/zipcode")
 public class ZipcodeController {
 
@@ -29,8 +32,15 @@ public class ZipcodeController {
 	@GetMapping("/search")
 	public ResponseEntity<?> searchAddress(AddressEntity addressForm) throws IOException {
 
+		if(Objects.isNull(addressForm.getZipcode())) {
+			ErrorDetail errorDetail = new ErrorDetail(HttpStatus.BAD_REQUEST.value(), "400", "郵便番号が入力されていません",
+					"郵便番号が入力入力されていません", "");
+			ErrorResponse errorResponse = new ErrorResponse(errorDetail);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);			
+		}
+		
 		if (!addressForm.getZipcode().matches("\\d{7}")) {
-			ErrorDetail errorDetail = new ErrorDetail(HttpStatus.BAD_REQUEST.value(), "000", "7桁数字以外の値が指定されています",
+			ErrorDetail errorDetail = new ErrorDetail(HttpStatus.BAD_REQUEST.value(), "400", "7桁数字以外の値が指定されています",
 					"7桁数字以外の値が指定されています", "");
 			ErrorResponse errorResponse = new ErrorResponse(errorDetail);
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
@@ -40,7 +50,7 @@ public class ZipcodeController {
 			return ResponseEntity.ok().body(zipcodeService.searchAddress(addressForm));
 		} catch (Throwable e) {
 			logger.error(e.getMessage(), e);
-			ErrorDetail errorDetail = new ErrorDetail(HttpStatus.INTERNAL_SERVER_ERROR.value(), "000", "問題が発生しました",
+			ErrorDetail errorDetail = new ErrorDetail(HttpStatus.INTERNAL_SERVER_ERROR.value(), "400", "問題が発生しました",
 					"問題が発生しました", "");
 			ErrorResponse errorResponse = new ErrorResponse(errorDetail);
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
