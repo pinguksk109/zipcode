3db354b8150adf5c90c43b6ade09efc856f37bc6
Keisuke Ota <k.ota109@outlook.com>
2023-08-06 14:17:56 +0900

    DBから値を取り出すRepositoryクラスのテストを実装

--- src/test/java/jp/co/ot/zipcode/infrastructure/repository/ZipcodeDbRepositoryTest.java
+++ src/test/java/jp/co/ot/zipcode/infrastructure/repository/ZipcodeDbRepositoryTest.java
@@ -1,11 +1,14 @@
 package jp.co.ot.zipcode.infrastructure.repository;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import jp.co.ot.zipcode.domain.model.Zipcode;
 import jp.co.ot.zipcode.domain.model.response.ZipcodeDataDto;
 
 //@SpringBootTest
@@ -17,9 +20,21 @@ public class ZipcodeDbRepositoryTest {
 	
     @Test
     @Ignore
-    public void saveZipcode_200を受け取った場合_DBに値が保存されていること() throws IOException { 
-    	ZipcodeDataDto dto = new ZipcodeDataDto("大阪府", "大阪市", "北区", "ｵｵｻｶﾌ", "ｵｵｻｶｼ", "ｷﾀｸ", "5300001");
-//    	sut.saveZipcode(dto);
+    public void saveZipcode_DBに値が保存されていること() throws IOException { 
+    	List<ZipcodeDataDto> dtoList = new ArrayList();
+    	ZipcodeDataDto dto = new ZipcodeDataDto("大阪府a", "大阪市", "北区", "ｵｵｻｶﾌ", "ｵｵｻｶｼ", "ｷﾀｸ", "5300001");
+    	dtoList.add(dto);
+    	sut.saveZipcode(dtoList);
   }
+    
+    @Test
+    @Ignore
+    public void getList_DBから値と取り出せること() throws IOException { 
+    	List<Zipcode> zipcodes = sut.getList();
+    	// DBに沢山データある為、結果を目視確認する
+    	for(Zipcode zipcode : zipcodes) {
+    		System.out.println(zipcode);
+    	}
+}
 
 }
